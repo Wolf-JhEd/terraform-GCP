dfdfc871742861c793120608f1cc4a8c596523bb
Wolf-JhEd <edwar.salas.987@gmail.com>
2019-09-12 00:50:28 -0500

    crear VM instacia y una red VPC

--- main.tf
+++ main.tf
@@ -1,29 +1,25 @@
 provider "google" {
-  credentias="${file("service-account.json")}"
+  credentials="${file("service_account.json")}"
   project = "ra-fia-usmp"
-  region ="us-centrall"
-  zone="us-centrall-c"
+  region ="us-central1"
+  zone="us-central1-c"
 }
-
+ 
 resource "google_compute_instance" "vm-instance" {
   name="terraform-instance"
   machine_type="f1-micro"
 
     boot_disk{
-        initialize_params
-        {
+        initialize_params{
             image="debian-cloud/debian-9"
         }
     }
-}
 
-network_interface{
-    # A default network is created for all GCP project
-    network="${goolgle_compute_network.vpc_network.sef_link}"
-    access_config={
-    }
+  network_interface{
+      # A default network is created for all GCP project
+      network="${google_compute_network.vpc_network.self_link}" 
+  }
 }
-
 resource "google_compute_network" "vpc_network"{
   name ="terraform-network"
   auto_create_subnetworks="true"
